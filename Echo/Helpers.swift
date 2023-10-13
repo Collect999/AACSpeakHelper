ab5e9b9bf351e9e32504ae5cedeefc1979f8b687
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-10-13 09:53:36 +0100

    Add Key to display

--- Echo/Helpers.swift
+++ Echo/Helpers.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 func getLanguageAndRegion(_ givenLocale: String) -> String {
     let currentLocale: Locale = .current
@@ -15,4 +16,28 @@ func getLanguageAndRegion(_ givenLocale: String) -> String {
 func getLanguage(_ givenLocale: String) -> String {
     let currentLocale: Locale = .current
     return currentLocale.localizedString(forLanguageCode: givenLocale) ?? "Unknown"
+}
+
+func keyToDisplay(_ key: KeyEquivalent) -> String {
+    let deleteKey = KeyEquivalent("\u{7F}")
+    
+    let keyMap: [KeyEquivalent: String] = [
+        deleteKey: "<DELETE>",
+        .upArrow: "<UP ARROW>",
+        .downArrow: "<DOWN ARROW>",
+        .leftArrow: "<LEFT ARROW>",
+        .rightArrow: "<RIGHT ARROW>",
+        .clear: "<CLEAR>",
+        .delete: "<BACKSPACE>",
+        .deleteForward: "<DELETE FORWARD>",
+        .end: "<END>",
+        .escape: "<ESC>",
+        .home: "<HOME>",
+        .pageUp: "<PAGE UP>",
+        .return: "<ENTER>",
+        .space: "<SPACE>",
+        .tab: "<TAB>"
+    ]
+    
+    return keyMap[key] ?? "Key: \"\(key.character.description)\""
 }
