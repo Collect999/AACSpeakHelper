41486f278442d5417ddbbf6a63f353cace96bec8
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-10-20 15:39:37 +0100

    [PATCH] Fix #9

--- Echo/Components/ScrollLock.swift
+++ Echo/Components/ScrollLock.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import SwiftUIIntrospect
 
 /***
     Renders a ScrollView and keeps the given UUID always in the center of the scroll area
@@ -18,13 +19,23 @@ struct ScrollLock<Content: View>: SwiftUI.View {
     var body: some View {
         ScrollViewReader { scrollControl in
             content
-                .onChange(of: selectedUUID) { newUUID in
+                .onChange(of: selectedUUID) {
                     withAnimation {
-                        scrollControl.scrollTo(newUUID, anchor: .center)
+                        scrollControl.scrollTo(selectedUUID, anchor: .center)
                     }
                 }.onAppear {
                     scrollControl.scrollTo(selectedUUID, anchor: .center)
                 }.scrollDisabled(true)
         }
+        .introspect(.scrollView, on: .iOS(.v17)) { scrollView in
+            // We need a shorter animation than default on scrollTo so that it
+            // always finishes the animation before the next one starts.
+            // There seems to be a bug where `scrollTo` ignores the animation
+            // passed into withAnimation. This is why we have to put this hack
+            // in to set the 'contentOffsetAnimationDuration' value.
+            // https://stackoverflow.com/a/68645170/3125540
+            scrollView.setValue(0.1, forKeyPath: "contentOffsetAnimationDuration")
+        }
     }
 }
+
