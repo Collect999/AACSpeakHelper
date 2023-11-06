5d47a11c8e0b50d95172814bdb2ccc8166a8eb8a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-11-06 10:25:45 +0000

    [MAJOR] Add contact us links

--- Echo/Pages/SettingsPage.swift
+++ Echo/Pages/SettingsPage.swift
@@ -8,18 +8,36 @@
 import Foundation
 import SwiftUI
 
-struct ExternalLinks: View {
+struct ExternalLinksSection: View {
+    var body: some View {
+        if
+            let docsUrl = URL(string: "https://docs.acecentre.org.uk/products/v/echo"),
+            let contactUrl = URL(string: "https://acecentre.org.uk/contact"),
+            let aboutUrl = URL(string: "https://acecentre.org.uk/about")
+        {
+            Section {
+                Link(String(
+                    localized: "Read Documentation",
+                    comment: "A link to the external documentation"
+                ), destination: docsUrl)
+                Link(String(
+                    localized: "Contact Us",
+                    comment: "A link to the contact page"
+                ), destination: contactUrl)
+                Link(String(
+                    localized: "About Us",
+                    comment: "A link to the about page"
+                ), destination: aboutUrl)
+            }
+        }
+    }
+}
+
+struct ExternalLinksForm: View {
     var body: some View {
         VStack {
             Form {
-                if let docsUrl = URL(string: "https://docs.acecentre.org.uk/products/v/echo") {
-                    Section {
-                        Link(String(
-                            localized: "Read Documentation",
-                            comment: "A link to the external documentation"
-                        ), destination: docsUrl)
-                    }
-                }
+                ExternalLinksSection()
             }
         }
     }
@@ -51,7 +69,7 @@ enum SettingsPath: CaseIterable, Identifiable {
         case .spelling: SpellingAndAlphabetPage()
         case .analytics: AnalyticsOptions()
         case .onboarding: OnboardingSettingsPage()
-        case .externalLinks: ExternalLinks()
+        case .externalLinks: ExternalLinksForm()
         }
     }
     
@@ -111,14 +129,7 @@ struct SettingsPagePhone: View {
                 })
             }
             
-            if let docsUrl = URL(string: "https://docs.acecentre.org.uk/products/v/echo") {
-                Section {
-                    Link(String(
-                        localized: "Read Documentation",
-                        comment: "A link to the external documentation"
-                    ), destination: docsUrl)
-                }
-            }
+            ExternalLinksSection()
         }
         .navigationBarBackButtonHidden(true)
         .navigationTitle(
