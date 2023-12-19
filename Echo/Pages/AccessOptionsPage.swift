7a99c69819be3056d5d93b2884b4d3c85c354621
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-12-19 11:05:59 +0000

    Add game controller settings page

--- Echo/Pages/AccessOptionsPage.swift
+++ Echo/Pages/AccessOptionsPage.swift
@@ -7,14 +7,18 @@
 
 import Foundation
 import SwiftUI
+import GameController
 
 struct AccessOptionsPage: View {
     @EnvironmentObject var accessOptions: AccessOptions
+    @EnvironmentObject var controllerManager: ControllerManager
     
     @State var showNewSwitchSheet = false
     
     @State var currentSwitch: Switch?
     
+    @State var currentController: CustomGameController?
+        
     var body: some View {
         Form {
             Section(content: {
@@ -93,6 +97,33 @@ struct AccessOptionsPage: View {
                 comment: "Footer description describing what switch access is"
                 )
             })
+            
+            Section(content: {
+                ForEach(controllerManager.controllers, id: \.id) { controller in
+                    Button(action: {
+                        currentController = controller
+                    }, label: {
+                        HStack {
+                            Label(controller.displayName, systemImage: "gamecontroller.fill")
+                            Spacer()
+                            Text("Map Buttons", comment: "The help text on the controller button")
+                                .foregroundStyle(.gray)
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(.gray)
+                        }
+                    })
+                }
+                if controllerManager.controllers.isEmpty {
+                    Text(
+                        "No game controller connect to device. Please connect a controller to access these settings",
+                        comment: "Notification of no game controllers connected"
+                    )
+                }
+            }, header: {
+                Text("Game Controllers", comment: "Header for game controller settings area")
+            }, footer: {
+                Text("Use a game controller to control Echo", comment: "Footer for game controller settings area")
+            })
         }
         .onDisappear {
             accessOptions.save()
@@ -108,6 +139,40 @@ struct AccessOptionsPage: View {
                 holdAction: currentSwitchObject.holdAction,
                 id: currentSwitchObject.id
             )
+        }
+        .sheet(item: $currentController) { currentControllerObject in
+            NavigationView {
+                Form {
+                    ForEach(currentControllerObject.buttons, id: \.id) { button in
+                        Section {
+                            Label(button.displayName, systemImage: button.symbolName)
+                            ControllerButtonSection(
+                                holdAction: button.holdAction,
+                                tapAction: button.tapAction,
+                                button: button
+                            )
+                        }
+        
+                    }
+                    
+                }
+                .toolbar {
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button(action: {
+                            currentController = nil
+                        }, label: {
+                            Text(
+                                "Save",
+                                comment: "The save toolbar button when saving controller mapping"
+                            )
+                        })
+                    }
+                }
+                .navigationTitle(currentControllerObject.displayName)
+            }
+            .onDisappear {
+                currentControllerObject.save()
+            }
         }
         
         .navigationTitle(
