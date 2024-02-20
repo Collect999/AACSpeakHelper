284f479fb3b110f6318d2ac58b825e6a105e4238
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2024-02-20 16:10:36 +0000

    [PATCH] Add adult ace vocab

--- Echo/Tree.swift
+++ Echo/Tree.swift
@@ -10,11 +10,13 @@ import Foundation
 enum Vocabulary: Int, CaseIterable, Identifiable {
     case basic
     case spelling
+    case adultAce
     
     var id: String {
         switch self {
         case .basic: return "Basic"
         case .spelling:  return "Spelling"
+        case .adultAce: return "Adult Ace"
         }
     }
     
@@ -24,6 +26,8 @@ enum Vocabulary: Int, CaseIterable, Identifiable {
             return Tree.Basic
         case .spelling:
             return Tree.SpellingTree
+        case .adultAce:
+            return Tree.AdultAce
         }
     }
 }
@@ -105,6 +109,101 @@ class Tree {
         self.name = name
         self.rootNode = rootNode
     }
+    
+    public static var AdultAce = Tree(
+        name: "Adult Ace",
+        rootNode: Node(type: .root, children: [
+            Node(type: .branch, text: "Phrases", children: [
+                Node(type: .phrase, text: "Yes"),
+                Node(type: .phrase, text: "No"),
+                Node(type: .phrase, text: "Help"),
+                Node(type: .phrase, text: "Stop"),
+                Node(type: .phrase, text: "Suction"),
+                Node(type: .phrase, text: "Hello"),
+                Node(type: .phrase, text: "Bye"),
+                Node(type: .phrase, text: "Thank you")
+            ]),
+            Node(type: .spelling, text: "I will spell it"),
+            Node(type: .branch, text: "Chat", children: [
+                Node(type: .branch, text: "Questions", children: [
+                    Node(type: .phrase, text: "How are you"),
+                    Node(type: .phrase, text: "When will I see you again"),
+                    Node(type: .branch, text: "Where is", children: [
+                        Node(type: .phrase, text: "Family"),
+                        Node(type: .phrase, text: "Staff")
+                    ])
+                ]),
+                Node(type: .phrase, text: "Thank you"),
+                Node(type: .phrase, text: "I don't want to talk")
+            ]),
+            Node(type: .branch, text: "Care", children: [
+                Node(type: .phrase, text: "Wash"),
+                Node(type: .phrase, text: "Shave"),
+                Node(type: .phrase, text: "Shower"),
+                Node(type: .phrase, text: "Change"),
+                Node(type: .phrase, text: "Clothes"),
+                Node(type: .phrase, text: "Pad"),
+                Node(type: .phrase, text: "Brush teeth"),
+                Node(type: .phrase, text: "Tracheostomy"),
+                Node(type: .phrase, text: "Suctioning"),
+                Node(type: .phrase, text: "Speech Valve"),
+                Node(type: .phrase, text: "Positioning"),
+                Node(type: .phrase, text: "Move me up"),
+                Node(type: .phrase, text: "Move me down"),
+            ]),
+            Node(type: .branch, text: "People", children: [
+                Node(type: .branch, text: "Family", children: [
+                    Node(type: .phrase, text: "Partner"),
+                    Node(type: .phrase, text: "Children"),
+                    Node(type: .phrase, text: "Parents")
+                ]),
+                Node(type: .branch, text: "Health professionals", children: [
+                    Node(type: .phrase, text: "Nurse"),
+                    Node(type: .phrase, text: "Doctor"),
+                    Node(type: .phrase, text: "O.T"),
+                    Node(type: .phrase, text: "Physio"),
+                    Node(type: .phrase, text: "S.L.T"),
+                    Node(type: .phrase, text: "Dentist"),
+                    Node(type: .phrase, text: "The care manager")
+                ]),
+                Node(type: .phrase, text: "Someone Else")
+            ]),
+            Node(type: .branch, text: "Environment", children: [
+                Node(type: .phrase, text: "Heating"),
+                Node(type: .phrase, text: "Turn up"),
+                Node(type: .phrase, text: "Turn down"),
+                Node(type: .phrase, text: "Window"),
+                Node(type: .phrase, text: "Open"),
+                Node(type: .phrase, text: "Closed"),
+                Node(type: .phrase, text: "Curtain"),
+                Node(type: .phrase, text: "Radio"),
+                Node(type: .phrase, text: "Lights"),
+                Node(type: .phrase, text: "Turn on"),
+                Node(type: .phrase, text: "Turn off"),
+                Node(type: .phrase, text: "Door"),
+                Node(type: .phrase, text: "TV"),
+                Node(type: .branch, text: "Change channel", children: [
+                    Node(type: .phrase, text: "BBC one"),
+                    Node(type: .phrase, text: "Audiobook")
+                ])
+            ]),
+            Node(type: .branch, text: "Feelings", children: [
+                Node(type: .phrase, text: "I'm tired"),
+                Node(type: .phrase, text: "I'm in pain"),
+                Node(type: .phrase, text: "Head"),
+                Node(type: .phrase, text: "Neck"),
+                Node(type: .phrase, text: "Chest"),
+                Node(type: .phrase, text: "Back"),
+                Node(type: .phrase, text: "Bum"),
+                Node(type: .phrase, text: "Legs"),
+                Node(type: .phrase, text: "Arms"),
+                Node(type: .phrase, text: "I'm okay thanks"),
+                Node(type: .phrase, text: "I'm bored"),
+                Node(type: .phrase, text: "I'm sad"),
+                Node(type: .phrase, text: "I'm angry")
+            ])
+        ])
+    )
     
     // periphery:ignore
     public static var Basic = Tree(
