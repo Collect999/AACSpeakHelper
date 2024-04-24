67e35b05e23fc3fbdd74835a0024728271ad88ef
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2024-04-24 12:24:15 +0100

    Add horizontal scroll lock

--- Echo/Components/ScrollLock.swift
+++ Echo/Components/ScrollLock.swift
@@ -9,6 +9,44 @@ import Foundation
 import SwiftUI
 import SwiftUIIntrospect
 
+struct HorizontalScrollLock<Content: View>: SwiftUI.View {
+    var selectedUUID: UUID
+    var locked: Bool = true
+    
+    @ViewBuilder var content: Content
+    
+    var body: some View {
+        ScrollViewReader { scrollControl in
+            ScrollView([.horizontal]) {
+                HStack {
+                    content
+                        .onChange(of: selectedUUID) {
+                            withAnimation {
+                                scrollControl.scrollTo("FINAL_ID", anchor: .trailing)
+                            }
+                        }.onAppear {
+                            scrollControl.scrollTo("FINAL_ID", anchor: .trailing)
+                        }
+                    ZStack {
+                    }
+                    .id("FINAL_ID")
+                }
+                
+            }
+            .scrollDisabled(locked)
+            .introspect(.scrollView, on: .iOS(.v17)) { scrollView in
+                // We need a shorter animation than default on scrollTo so that it
+                // always finishes the animation before the next one starts.
+                // There seems to be a bug where `scrollTo` ignores the animation
+                // passed into withAnimation. This is why we have to put this hack
+                // in to set the 'contentOffsetAnimationDuration' value.
+                // https://stackoverflow.com/a/68645170/3125540
+                scrollView.setValue(0.1, forKeyPath: "contentOffsetAnimationDuration")
+            }
+        }
+    }
+}
+
 /***
     Renders a ScrollView and keeps the given UUID always in the center of the scroll area
  */
--- Echo/ContentView.swift
+++ Echo/ContentView.swift
@@ -9,7 +9,7 @@ struct ContentView: SwiftUI.View {
     @EnvironmentObject var analytics: Analytics
     
     @State var lastLangId: String?
-
+    
     @Binding var showOnboarding: Bool
     
     @State var loading = true
@@ -18,134 +18,131 @@ struct ContentView: SwiftUI.View {
         if showOnboarding {
             Onboarding(endOnboarding: { pageNumber, finishType in
                 analytics.finishedOnboarding(pageNumber: pageNumber, finishType: finishType)
-            
+                
                 showOnboarding = false
             })
         } else {
             NavigationStack {
                 ZStack {
-                        if loading {
-                            VStack {
-                                ProgressView()
-                                Text("Echo is loading, thank you for waiting", comment: "Text shown on loading screen.")
+                    if loading {
+                        VStack {
+                            ProgressView()
+                            Text("Echo is loading, thank you for waiting", comment: "Text shown on loading screen.")
+                        }
+                    } else {
+                        ZStack {
+                            if accessOptions.showOnScreenArrows {
+                                OnScreenArrows(
+                                    up: { items.userPrevNode() },
+                                    down: { items.userNextNode() },
+                                    left: { items.userBack() },
+                                    right: { items.userClickHovered() }
+                                )
                             }
-                        } else {
-                            ZStack {
-                                if accessOptions.showOnScreenArrows {
-                                    OnScreenArrows(
-                                        up: { items.userPrevNode() },
-                                        down: { items.userNextNode() },
-                                        left: { items.userBack() },
-                                        right: { items.userClickHovered() }
-                                    )
-                                }
-                                if accessOptions.enableSwitchControl {
-                                    KeyPressController()
-                                }
-                                VStack {
+                            if accessOptions.enableSwitchControl {
+                                KeyPressController()
+                            }
+                            VStack {
+                                
+                                HStack {
                                     
-                                    HStack {
-                                        
-                                        GeometryReader { geoReader in
-                                            ScrollView([.horizontal]) {
+                                    GeometryReader { geoReader in
+                                        HorizontalScrollLock(selectedUUID: items.hoveredNode.id) {
+                                            ForEach(items.getLevels(), id: \.self) { currentLevel in
                                                 HStack {
-                                                    ForEach(items.getLevels(), id: \.self) { currentLevel in
-                                                        HStack {
-                                                            HStack {
-                                                                if currentLevel.last {
-                                                                    Image(systemName: "chevron.right")
-                                                                }
-                                                            }.frame(minWidth: 25)
-                                                            ScrollLock(selectedUUID: currentLevel.hoveredNode.id) {
-                                                                ScrollView {
-                                                                    VStack(alignment: .leading) {
-                                                                        ForEach(currentLevel.nodes) { node in
-                                                                            HStack {
-                                                                                if node.id == currentLevel.hoveredNode.id {
-                                                                                    Text(node.displayText)
-                                                                                        .padding()
-                                                                                        .bold()
-                                                                                        .opacity(currentLevel.last ? 1 : 0.5)
-                                                                                    
-                                                                                } else {
-                                                                                    Text(node.displayText)
-                                                                                        .padding()
-                                                                                        .opacity(currentLevel.last ? 1 : 0.5)
-                                                                                            
-                                                                                }
-                                                                            }.id(node.id)
+                                                    HStack {
+                                                        if currentLevel.last {
+                                                            Image(systemName: "chevron.right")
+                                                        }
+                                                    }.frame(minWidth: 25)
+                                                    ScrollLock(selectedUUID: currentLevel.hoveredNode.id) {
+                                                        ScrollView {
+                                                            VStack(alignment: .leading) {
+                                                                ForEach(currentLevel.nodes) { node in
+                                                                    HStack {
+                                                                        if node.id == currentLevel.hoveredNode.id {
+                                                                            Text(node.displayText)
+                                                                                .padding()
+                                                                                .bold()
+                                                                                .opacity(currentLevel.last ? 1 : 0.5)
+                                                                            
+                                                                        } else {
+                                                                            Text(node.displayText)
+                                                                                .padding()
+                                                                                .opacity(currentLevel.last ? 1 : 0.5)
                                                                             
                                                                         }
-                                                                    }.padding(.vertical, geoReader.size.height/2)
+                                                                    }.id(node.id)
                                                                     
                                                                 }
-                                                                
-                                                            }
+                                                            }.padding(.vertical, geoReader.size.height/2)
+                                                            
                                                         }
-                                                        .frame(maxWidth: geoReader.size.width / 2)
+                                                        
                                                     }
                                                 }
+                                                .frame(maxWidth: geoReader.size.width / 2)
                                             }
-                                            .scrollDisabled(true)
-                                            .defaultScrollAnchor(.trailing)
+                                            
                                         }
                                     }
-                                    .background(Color("transparent"))
-                                    .contentShape(Rectangle())
-                                    .padding()
-                                    .onTapGesture(count: 1, perform: {
+                                }
+                                .background(Color("transparent"))
+                                .contentShape(Rectangle())
+                                .padding()
+                                .onTapGesture(count: 1, perform: {
+                                    if accessOptions.allowSwipeGestures {
+                                        items.userClickHovered()
+                                        analytics.userInteraction(type: "Tap", extraInfo: "Single")
+                                    }
+                                })
+                                // Note the directions given here refer to the direction of inertia. So 'left' is swiping your finger from right to the left
+                                .swipe(
+                                    up: {
+                                        if accessOptions.allowSwipeGestures {
+                                            items.userNextNode()
+                                            analytics.userInteraction(type: "Swipe", extraInfo: "UP")
+                                        }
+                                    },
+                                    down: {
+                                        if accessOptions.allowSwipeGestures {
+                                            items.userPrevNode()
+                                            analytics.userInteraction(type: "Swipe", extraInfo: "DOWN")
+                                        }
+                                    },
+                                    left: {
+                                        if accessOptions.allowSwipeGestures {
+                                            items.userBack()
+                                            analytics.userInteraction(type: "Swipe", extraInfo: "LEFT")
+                                        }
+                                    },
+                                    right: {
                                         if accessOptions.allowSwipeGestures {
                                             items.userClickHovered()
-                                            analytics.userInteraction(type: "Tap", extraInfo: "Single")
+                                            analytics.userInteraction(type: "Swipe", extraInfo: "RIGHT")
                                         }
-                                    })
-                                    // Note the directions given here refer to the direction of inertia. So 'left' is swiping your finger from right to the left
-                                    .swipe(
-                                        up: {
-                                            if accessOptions.allowSwipeGestures {
-                                                items.userNextNode()
-                                                analytics.userInteraction(type: "Swipe", extraInfo: "UP")
-                                            }
-                                        },
-                                        down: {
-                                            if accessOptions.allowSwipeGestures {
-                                                items.userPrevNode()
-                                                analytics.userInteraction(type: "Swipe", extraInfo: "DOWN")
-                                            }
-                                        },
-                                        left: {
-                                            if accessOptions.allowSwipeGestures {
-                                                items.userBack()
-                                                analytics.userInteraction(type: "Swipe", extraInfo: "LEFT")
-                                            }
-                                        },
-                                        right: {
-                                            if accessOptions.allowSwipeGestures {
-                                                items.userClickHovered()
-                                                analytics.userInteraction(type: "Swipe", extraInfo: "RIGHT")
-                                            }
-                                        }
-                                    )
-                                    
-                                    VStack {
-                                        Text(items.enteredText == "" ? " " : items.enteredText)
-                                            .padding(10)
-                                            .font(.system(size: CGFloat(spelling.messageBarFontSize)))
                                     }
-                                    .frame(maxWidth: .infinity)
-                                    .background(Color("lightGray"))
-                                    .shadow(radius: 1)
-                                    
-                                }
-                            }
-                            
-                            .accessibilityRepresentation(representation: {
-                                Text(
-                                    "Echo does not currently support system accessibility controls. To use Echo please disable your system accessibility controls. We hope to improve this in the future.",
-                                    comment: "An explaination to users using Voice over about how to use Echo"
                                 )
-                            })
+                                
+                                VStack {
+                                    Text(items.enteredText == "" ? " " : items.enteredText)
+                                        .padding(10)
+                                        .font(.system(size: CGFloat(spelling.messageBarFontSize)))
+                                }
+                                .frame(maxWidth: .infinity)
+                                .background(Color("lightGray"))
+                                .shadow(radius: 1)
+                                
+                            }
                         }
+                        
+                        .accessibilityRepresentation(representation: {
+                            Text(
+                                "Echo does not currently support system accessibility controls. To use Echo please disable your system accessibility controls. We hope to improve this in the future.",
+                                comment: "An explaination to users using Voice over about how to use Echo"
+                            )
+                        })
+                    }
                     
                 }
                 .navigationTitle(
@@ -200,6 +197,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some SwiftUI.View {
         ContentView(showOnboarding: .constant(false)).preferredColorScheme(.light)
         ContentView(showOnboarding: .constant(false)).preferredColorScheme(.dark)
-
+        
     }
 }
