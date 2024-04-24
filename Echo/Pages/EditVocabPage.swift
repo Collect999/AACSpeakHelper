3530921c09faea815f0cd24ce10432c420876801
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2024-04-24 17:02:32 +0100

    [PATCH] Add style to editing page

--- Echo/Pages/EditVocabPage.swift
+++ Echo/Pages/EditVocabPage.swift
@@ -7,12 +7,29 @@
 
 import SwiftUI
 
+struct SheetButton<SheetLabel: View, SheetContent: View>: View {
+    @ViewBuilder var sheetLabel: SheetLabel
+    @ViewBuilder var sheetContent: SheetContent
+    
+    @State var isPresented = false
+    
+    var body: some View {
+        Button(action: {
+            isPresented = true
+        }, label: {
+            sheetLabel
+        }).sheet(isPresented: $isPresented, content: {
+            sheetContent
+        })
+    }
+}
+
 struct EditVocabPage: View {
     @EnvironmentObject var items: ItemsList
     @EnvironmentObject var accessOptions: AccessOptions
     @EnvironmentObject var spelling: SpellingOptions
     @EnvironmentObject var analytics: Analytics
-    
+        
     var body: some SwiftUI.View {
         ZStack {
             
@@ -30,87 +47,95 @@ struct EditVocabPage: View {
                     GeometryReader { geoReader in
                         HorizontalScrollLock(selectedUUID: items.hoveredNode.id, locked: false) {
                             
-                                ForEach(items.getLevels(), id: \.self) { currentLevel in
-                                    HStack {
-                                        
-                                        ScrollLock(selectedUUID: currentLevel.hoveredNode.id, locked: false) {
-                                            ScrollView {
-                                                VStack(alignment: .leading) {
-                                                    ForEach(currentLevel.nodes) { node in
-                                                        HStack {
-                                                            if node.id == items.hoveredNode.id {
-                                                                VStack {
-                                                                    Button(action: {
-                                                                        print("Add above")
-                                                                    }, label: {
-                                                                        Image(systemName: "plus.circle")
-                                                                            .foregroundStyle(.green)
+                            ForEach(items.getLevels(), id: \.self) { currentLevel in
+                                HStack {
+                                    
+                                    ScrollLock(selectedUUID: currentLevel.hoveredNode.id, locked: false) {
+                                        ScrollView {
+                                            VStack(alignment: .leading) {
+                                                ForEach(currentLevel.nodes) { node in
+                                                    HStack {
+                                                        if node.id == items.hoveredNode.id {
+                                                            VStack {
+                                                                Button(action: {
+                                                                    print("Add above")
+                                                                }, label: {
+                                                                    Image(systemName: "plus.circle")
+                                                                        .foregroundStyle(.green)
+                                                                })
+                                                                
+                                                                    
+                                                                HStack {
+                                                                    
+                                                                    SheetButton(sheetLabel: {
+                                                                        Image(systemName: "gear")
+                                                                    }, sheetContent: {
+                                                                        Text(node.displayText)
                                                                     })
                                                                     
-                                                                    HStack {
-                                                                        VStack {
-                                                                            TextField(
-                                                                                "temp",
-                                                                                text: .constant(node.displayText)
-                                                                            )
-                                                                            Color.gray.frame(height: 1)
-                                                                            
-                                                                        }
-                                                                        .padding()
-                                                                        .background(.red)
-                                                                        .cornerRadius(6)
-                                                                        .padding()
-                                                                        
-                                                                        if node.children.count == 0 {
-                                                                            Button(action: {
-                                                                                print("Add next")
-                                                                            }, label: {
-                                                                                Image(systemName: "plus.circle")
-                                                                                    .foregroundStyle(.green)
-                                                                            })
-                                                                        } else {
-                                                                            Button(action: {
-                                                                                print("Add next")
-                                                                            }, label: {
-                                                                                Image(systemName: "chevron.right")
-                                                                            })
-                                                                        }
+                                                                    TextField(
+                                                                        "temp",
+                                                                        text: .constant(node.displayText)
+                                                                    )
+                                                                    .frame(minWidth: 200)
+                                                                    .textFieldStyle(.roundedBorder)
+                                                                    .cornerRadius(6)
+                                                                    
+                                                                    if node.children.count == 0 {
+                                                                        Button(action: {
+                                                                            print("Add next")
+                                                                        }, label: {
+                                                                            Image(systemName: "plus.circle")
+                                                                                .foregroundStyle(.green)
+                                                                        })
+                                                                    } else {
+                                                                        Button(action: {
+                                                                            items.userClickHovered()
+                                                                        }, label: {
+                                                                            Image(systemName: "chevron.right")
+                                                                        })
                                                                     }
-                                                                    
-                                                                    Button(action: {
-                                                                        print("Add below")
-                                                                    }, label: {
-                                                                        Image(systemName: "plus.circle")
-                                                                            .foregroundStyle(.green)
-                                                                    })
                                                                 }
                                                                 
                                                                 
-                                                                
-                                                                
-                                                            } else {
                                                                 Button(action: {
-                                                                    print("Pressed")
-                                                                    items.hoverNode(node, shouldScan: false)
+                                                                    print("Add below")
                                                                 }, label: {
-                                                                    Text(node.displayText)
-                                                                        .padding()
-                                                                        .opacity(currentLevel.last ? 1 : 0.5)
+                                                                    Image(systemName: "plus.circle")
+                                                                        .foregroundStyle(.green)
                                                                 })
-                                                                
-                                                                
                                                             }
-                                                        }.id(node.id)
-                                                        
-                                                    }
-                                                }.padding(.vertical, geoReader.size.height/2)
-                                                
-                                            }
+                                                            .padding(8)
+                                                            .background(.lightGray)
+                                                            .cornerRadius(6)
+                                                            .shadow(radius: 5)
+                                                            .padding(8)
+                                                            
+                                                            
+                                                            
+                                                        } else {
+                                                            Button(action: {
+                                                                print("Pressed")
+                                                                items.hoverNode(node, shouldScan: false)
+                                                            }, label: {
+                                                                Text(node.displayText)
+                                                                    .padding()
+                                                                    .opacity(currentLevel.last ? 1 : 0.5)
+                                                            })
+                                                            
+                                                            
+                                                        }
+                                                    }.id(node.id)
+                                                    
+                                                }
+                                            }.padding(.vertical, geoReader.size.height/2)
                                             
                                         }
+                                        
                                     }
-                                    .frame(maxWidth: geoReader.size.width / 2)
                                 }
+                                .frame(maxWidth: geoReader.size.width / 2)
+                            }
                             
                         }
                     }
