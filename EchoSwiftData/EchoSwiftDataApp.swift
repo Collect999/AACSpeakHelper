e98aac77e5441b849e7574fdc3b23ae06db0c65f
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2024-05-23 16:57:13 +0100

    Setup new app

---
+++ EchoSwiftData/EchoSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoSwiftDataApp.swift
+//  EchoSwiftData
+//
+//  Created by Gavin Henderson on 23/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct EchoSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
