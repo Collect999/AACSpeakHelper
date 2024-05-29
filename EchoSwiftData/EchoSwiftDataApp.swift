c147cbfac120f7ad270d3c64c670b72a40ffd31f
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2024-05-29 14:02:36 +0100

    Add some comments

--- EchoSwiftData/EchoSwiftDataApp.swift
+++ EchoSwiftData/EchoSwiftDataApp.swift
@@ -15,37 +15,42 @@ struct EchoSwiftDataApp: App {
             SwiftDataInitialiser()
         }.modelContainer(for: Settings.self) { result in
             do {
+                /*
+                 Create the initial settings object if it does not exist
+                 */
                 let container = try result.get()
-
                 let allSettings = try container.mainContext.fetch(FetchDescriptor<Settings>())
-                
                 var currentSettings = Settings()
-                
                 if let firstSettings = allSettings.first {
                     currentSettings = firstSettings
                 } else {
                     container.mainContext.insert(currentSettings)
                 }
-                
                 try container.mainContext.save()
                 
+                /*
+                 Insert the system vocabularies, this performs an upsert so won't create any duplicates
+                 */
                 for newSystemVocab in Vocabulary.getSystemVocabs() {
-                    // This will upsert because we are reusing the same slug
                     container.mainContext.insert(newSystemVocab)
                 }
-                
                 try container.mainContext.save()
                 
-                let defaultVocab = try container.mainContext.fetch(FetchDescriptor<Vocabulary>(
-                    predicate: #Predicate {
-                        $0.isDefault == true
-                    }
-                ))
                 
-                if let unwrappedVocab = defaultVocab.first, currentSettings.currentVocab == nil {
-                    currentSettings.currentVocab = unwrappedVocab
+                /*
+                 Set the default vocab if there is no vocab
+                 */
+                if currentSettings.currentVocab == nil {
+                    let defaultVocab = try container.mainContext.fetch(FetchDescriptor<Vocabulary>(
+                        predicate: #Predicate {
+                            $0.isDefault == true
+                        }
+                    ))
                     
-                    try container.mainContext.save()
+                    if let unwrappedVocab = defaultVocab.first {
+                        currentSettings.currentVocab = unwrappedVocab
+                        try container.mainContext.save()
+                    }
                 }
                 
             } catch {
