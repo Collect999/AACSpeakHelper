34792d8e945836223582e068446df929d1ab9351
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2024-06-27 14:43:55 +0100

    Use new refactored switch section

--- EchoSwiftData/Components/Onboarding/SwitchOnboarding.swift
+++ EchoSwiftData/Components/Onboarding/SwitchOnboarding.swift
@@ -10,12 +10,7 @@ import SwiftUI
 import SwiftData
 
 struct SwitchOnboarding: View {
-    @State var showNewSwitchSheet = false
-    @State var currentSwitch: Switch?
-
     @Environment(Settings.self) var settings: Settings
-
-    @Query var switches: [Switch]
     
     var body: some View {
         @Bindable var settingsBindable = settings
@@ -51,49 +46,9 @@ struct SwitchOnboarding: View {
                 )
             )
             Form {
-                Section(content: {
-                    Toggle(
-                        String(
-                            localized: "Switch Control",
-                            comment: "Label for toggle to turn switch control off and on"
-                        ),
-                        isOn: $settingsBindable.enableSwitchControl
-                    )
-                    
-                    ForEach(switches) { currentLoopSwitch in
-                        Button(action: {
-                            currentSwitch = currentLoopSwitch
-                            showNewSwitchSheet.toggle()
-                        }, label: {
-                            HStack {
-                                Text(currentLoopSwitch.name)
-                                Spacer()
-                                Text(keyToDisplay(currentLoopSwitch.key))
-                                    .foregroundStyle(.gray)
-                                Image(systemName: "chevron.right")
-                                    .foregroundStyle(.gray)
-                            }
-                        })
-                    }
-                    
-                    Button(action: {
-                        showNewSwitchSheet.toggle()
-                    }, label: {
-                        Label(
-                            String(
-                                localized: "Add Switch",
-                                comment: "Button label to add a new switch to the list"
-                            ),
-                            systemImage: "plus.circle.fill"
-                        )
-                    })
-                })
-                
+                SwitchControlSection()
             }
             .scrollContentBackground(.hidden)
-            AddSwitchSheet(
-                showNewSwitchSheet: $showNewSwitchSheet, currentSwitch: $currentSwitch
-            )
         }
         
     }
