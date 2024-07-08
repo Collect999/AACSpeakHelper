233c30dd0f4accb2b1abae3c25e78459527dfb2f
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2024-07-08 11:53:45 +0100

    Clamp values

---
+++ EchoSwiftData/Extensions/Comparable.swift
@@ -0,0 +1,19 @@
+//
+//  Comparable.swift
+//  EchoSwiftData
+//
+//  Created by Gavin Henderson on 05/07/2024.
+//
+
+import Foundation
+
+extension Comparable {
+    func clamped(_ f: Self, _ t: Self) -> Self {
+        var r = self
+        if r < f { r = f }
+        if r > t { r = t }
+        // (use SIMPLE, EXPLICIT code here to make it utterly clear
+        // whether we are inclusive, what form of equality, etc etc)
+        return r
+    }
+}
